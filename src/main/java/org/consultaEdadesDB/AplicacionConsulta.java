bae70ded7fc4b86df541acf5b328a364e781d843
David Torres Asus <davidtorresdev7@gmail.com>
2024-10-07 11:48:51 -0500

    first commit

---
+++ src/main/java/org/consultaEdadesDB/AplicacionConsulta.java
@@ -0,0 +1,12 @@
+package org.consultaEdadesDB;
+
+import javax.swing.*;
+
+public class AplicacionConsulta {
+
+    public static void main(String[] args) {
+
+        JFrame marcoAplicacion = new MarcoAplicacion();
+
+    }
+}
